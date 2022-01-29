a6a4db3f8a0e1964613f1b7fd8514e06ebf329c8
Artur <coungard@mail.ru>
2022-01-29 16:13:17 +0300

    project

---
+++ src/test/java/com/coungard/crypto/CryptoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.coungard.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
